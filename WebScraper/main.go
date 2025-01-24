a632e16a684b178e30a4ca9b6e303a0062a2f796
Huseyin <seyitayaga6363@gmail.com>
2025-01-24 22:10:47 +0300

    Web Scraper Updated

--- WebScraper/main.go
+++ WebScraper/main.go
@@ -13,7 +13,6 @@ import (
 )
 
 func main() {
-
 	outputDir := "scraped_data"
 
 	err := os.MkdirAll(outputDir, os.ModePerm)
@@ -26,7 +25,7 @@ func main() {
 		fmt.Println("\n--- Web Scraper Menü ---")
 		fmt.Println("1: The Hacker News'ten veri çek ve kaydet")
 		fmt.Println("2: Cyrops'tan veri çek ve kaydet")
-		fmt.Println("3: Yavuzlardan veri çek ve kaydet")
+		fmt.Println("3: BBC'den veri çek ve kaydet")
 		fmt.Println("q: Çıkış")
 		fmt.Print("Bir seçenek girin: ")
 
@@ -36,13 +35,13 @@ func main() {
 		switch choice {
 		case "1":
 			fmt.Println("\nThe Hacker News'ten veri çekiliyor...")
-			scrapeAndSave("https://thehackernews.com/", filepath.Join(outputDir, "thehackernews.txt"))
+			scrapeHackerNews(filepath.Join(outputDir, "thehackernews.txt"))
 		case "2":
 			fmt.Println("\nCyrops'tan veri çekiliyor...")
-			scrapeAndSave("https://cyrops.com/", filepath.Join(outputDir, "cyrops.txt"))
+			scrapeCyrops(filepath.Join(outputDir, "cyrops.txt"))
 		case "3":
-			fmt.Println("\nYavuzlar'dan veri çekiliyor...")
-			scrapeAndSave("https://yavuzlar.com/", filepath.Join(outputDir, "yavuzlar.txt"))
+			fmt.Println("\nBBC'den veri çekiliyor...")
+			scrapeBBC(filepath.Join(outputDir, "bbc.txt"))
 		case "q":
 			fmt.Println("\nÇıkış yapılıyor...")
 			os.Exit(0)
@@ -52,8 +51,8 @@ func main() {
 	}
 }
 
-func scrapeAndSave(url string, filePath string) {
-
+func scrapeHackerNews(filePath string) {
+	url := "https://thehackernews.com/"
 	res, err := http.Get(url)
 	if err != nil {
 		log.Fatal(err)
@@ -65,21 +64,101 @@ func scrapeAndSave(url string, filePath string) {
 		log.Fatal(err)
 	}
 
-	title := doc.Find("title").Text()
-	if title == "" {
-		title = "Başlık bulunamadı"
+	var data []string
+	doc.Find(".body-post").Each(func(i int, s *goquery.Selection) {
+		title := strings.TrimSpace(s.Find("h2.home-title").Text())
+		description := strings.TrimSpace(s.Find(".home-desc").Text())
+		time := strings.TrimSpace(s.Find(".item-label span.h-datetime").Text())
+
+		if title == "" {
+			title = "Başlık bulunamadı"
+		}
+		if description == "" {
+			description = "Açıklama bulunamadı"
+		}
+		if time == "" {
+			time = "Zaman bilgisi bulunamadı"
+		}
+
+		postData := fmt.Sprintf("Başlık: %s\nAçıklama: %s\nZaman: %s\n", title, description, time)
+		data = append(data, postData)
+	})
+
+	saveToFile(filePath, data)
+}
+
+func scrapeCyrops(filePath string) {
+	url := "https://cyrops.com/"
+	res, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer res.Body.Close()
+
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	if err != nil {
+		log.Fatal(err)
 	}
 
-	description, _ := doc.Find("meta[name='description']").Attr("content")
-	if description == "" {
-		description = "Açıklama bulunamadı"
-	}
+	var data []string
+	doc.Find(".elementor-column").Each(func(i int, s *goquery.Selection) {
+		title := strings.TrimSpace(s.Find(".elementor-widget-wrap .elementor-element .elementor-widget-container .tt-icon-box__content h3.tt-icon-box__title").Text())
+		description := strings.TrimSpace(s.Find(".elementor-widget-wrap .elementor-element .elementor-widget-container .tt-icon-box__content p.tt-icon-box__description").Text())
+		time := strings.TrimSpace(s.Find(".elementor-widget-wrap .elementor-element .elementor-widget-container .tt-icon-box__content .tt-icon-box__time").Text())
+		// cyrops.com da zaman bilgisi bulunmuyor bu yüzden boş dönecektir.
 
-	date := doc.Find("time").Text()
-	if date == "" {
-		date = doc.Find("meta[name='publish-date']").AttrOr("content", "Tarih bulunamadı")
-	}
+		if title == "" {
+			title = "Başlık bulunamadı"
+		}
+		if description == "" {
+			description = "Açıklama bulunamadı"
+		}
+		if time == "" {
+			time = "Zaman bilgisi bulunamadı"
+		}
+		postData := fmt.Sprintf("Başlık: %s\nAçıklama: %s\nZaman: %s\n", title, description, time)
+		data = append(data, postData)
 
+	})
+	saveToFile(filePath, data)
+}
+
+func scrapeBBC(filePath string) {
+	url := "https://bbc.com/turkce"
+	res, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer res.Body.Close()
+
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	var data []string
+	doc.Find(".bbc-1rrncb9").Each(func(i int, s *goquery.Selection) {
+		s.Find("li").Each(func(i int, s *goquery.Selection) {
+			title := strings.TrimSpace(s.Find(".promo-text h3").Text())
+			description := strings.TrimSpace(s.Find(".promo-text p").Text())
+			time := strings.TrimSpace(s.Find(".promo-text time").Text())
+
+			if title == "" {
+				title = "Başlık bulunamadı"
+			}
+			if description == "" {
+				description = "Açıklama bulunamadı"
+			}
+			if time == "" {
+				time = "Zaman bilgisi bulunamadı"
+			}
+			postData := fmt.Sprintf("Başlık: %s\nAçıklama: %s\nZaman: %s\n", title, description, time)
+			data = append(data, postData)
+		})
+	})
+	saveToFile(filePath, data)
+}
+
+func saveToFile(filePath string, items []string) {
 	file, err := os.Create(filePath)
 	if err != nil {
 		log.Fatal(err)
@@ -87,9 +166,10 @@ func scrapeAndSave(url string, filePath string) {
 	defer file.Close()
 
 	writer := bufio.NewWriter(file)
-	fmt.Fprintf(writer, "Başlık: %s\n", title)
-	fmt.Fprintf(writer, "Açıklama: %s\n", description)
-	fmt.Fprintf(writer, "Tarih: %s\n", date)
+	for _, item := range items {
+		fmt.Fprintln(writer, item)
+		fmt.Fprintln(writer, "------------------------------------------------------------")
+	}
 	writer.Flush()
 
 	fmt.Printf("Veriler %s dosyasına başarıyla kaydedildi.\n", filePath)
